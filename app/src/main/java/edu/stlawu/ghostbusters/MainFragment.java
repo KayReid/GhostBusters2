57c814431d3ec921d606cb66b214f58c46806bf1
AnnaBreton <32850138+AnnaBreton@users.noreply.github.com>
2018-11-02 11:01:45 -0400

    Anna Changes

---
+++ app/src/main/java/edu/stlawu/ghostbusters/MainFragment.java
@@ -0,0 +1,81 @@
+package edu.stlawu.ghostbusters;
+
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.Objects;
+
+public class MainFragment extends Fragment {
+
+    public static final String PREF_NAME = "MontyHall";
+    public static final String NEW_CLICKED = "NEWCLICKED";
+
+    private OnFragmentInteractionListener FIListener;
+
+    public MainFragment() {
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        View aboutButton = rootView.findViewById(R.id.about_button);
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder =
+                        new AlertDialog.Builder(getActivity());
+                builder.setTitle(R.string.about_title_text);
+                builder.setMessage(R.string.about);
+                AlertDialog.Builder builder1 = builder.setPositiveButton(R.string.ok,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog,
+                                                int which) {
+                            }
+                        });
+                builder.show();
+            }
+        });
+
+
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            FIListener = (OnFragmentInteractionListener) context;
+        }
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(Uri uri);
+    }
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        FIListener = null;
+    }
+}
